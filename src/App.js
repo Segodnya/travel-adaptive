654a56d4523d663fafcd83bea27c0d0e02ecc54e
Kirill Kurentsov <immortalsteel@gmail.com>
2023-10-14 11:56:58 +0300

    feat: basic markup for 320p

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom';
+import icon from './images/app-icon.svg';
 import './App.css';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <header className="header">
+        <Link to="#" className="header__link">
+          <img src={icon} alt="app-icon" className="header__logo" />
+        </Link>
+        <button className="header__button" />
+      </header>
+      <main className="main">
+        <h1 className="main__title">Путешествовать — круто!</h1>
+        <button className="main__button">Поехали с нами</button>
+      </main>
+    </div>
+  );
 }
 
 export default App;
